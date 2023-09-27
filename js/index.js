1b3c2bdbbd7137e8408ca8c2fdc712cdda91c571
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-09-27 05:36:43 -0300

    update

--- js/index.js
+++ js/index.js
@@ -3,12 +3,15 @@ const text = document.querySelector(".sec-text");
 let textArray = ['Developers', 'Oradores', "Codo a Codo"]
 let count = 0;
 const textLoad = () => {
+    text.textContent = textArray[2]
     setInterval(() => {
         text.textContent = textArray[count]
         count++
         if (count > textArray.length - 1) {
             count = 0;
         }
+
     }, 4000)
 }
+
 textLoad()
